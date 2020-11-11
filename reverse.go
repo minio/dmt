53ad9734a00766b491b9fb0025d5e67709c1b0e3
Harshavardhana <harsha@minio.io>
2020-11-10 17:20:40 -0800

    add accessKey to tenant mapping

--- reverse.go
+++ reverse.go
@@ -4,19 +4,22 @@ import (
 	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	xhttp "github.com/minio/minio/cmd/http"
 	"github.com/minio/minio/cmd/rest"
+	"github.com/minio/minio/pkg/auth"
 	"golang.org/x/net/http2"
 )
 
@@ -68,9 +71,9 @@ var (
 
 func init() {
 	flag.StringVar(&caCert, "ca-cert", "/etc/nginx/ssl/ca.crt", "CA certificates")
-	flag.StringVar(&tlsCert, "tls-cert", "/etc/nginx/ssl/tls.crt", "TLS certificate")
 	flag.StringVar(&tlsKey, "tls-key", "/etc/nginx/ssl/tls.key", "TLS key")
-	flag.StringVar(&backend, "backend", "https://minio:9001", "MinIO backend")
+	flag.StringVar(&tlsCert, "tls-cert", "/etc/nginx/ssl/tls.crt", "TLS certificate")
+	flag.StringVar(&backend, "backend", "/etc/default/reverse.json", "MinIO deployment file")
 	globalDNSCache = xhttp.NewDNSCache(3*time.Second, 10*time.Second)
 }
 
@@ -101,20 +104,123 @@ func newInternodeHTTPTransport(tlsConfig *tls.Config, dialTimeout time.Duration)
 	}
 }
 
+var tenantAccessMapping = map[string]string{}
+
+// isValidRegion - verify if incoming region value is valid with configured Region.
+func isValidRegion(reqRegion string, confRegion string) bool {
+	if confRegion == "" {
+		return true
+	}
+	if confRegion == "US" {
+		confRegion = ""
+	}
+	// Some older s3 clients set region as "US" instead of
+	// globalMinioDefaultRegion, handle it.
+	if reqRegion == "US" {
+		reqRegion = ""
+	}
+	return reqRegion == confRegion
+}
+
+// parse credentialHeader string into its structured form.
+func parseCredentialHeader(credElement string, region string) (accessKey string, err error) {
+	creds := strings.SplitN(strings.TrimSpace(credElement), "=", 2)
+	if len(creds) != 2 {
+		return "", errors.New("error missing fields")
+	}
+	if creds[0] != "Credential" {
+		return "", errors.New("missing Credential tag")
+	}
+	credElements := strings.Split(strings.TrimSpace(creds[1]), "/")
+	if len(credElements) < 5 {
+		return "", errors.New("malformed Credential tag")
+	}
+	accessKey = strings.Join(credElements[:len(credElements)-4], "/") // The access key may contain one or more `/`
+	if !auth.IsAccessKeyValid(accessKey) {
+		return "", errors.New("invalid access key id")
+	}
+
+	credElements = credElements[len(credElements)-4:]
+	if _, err = time.Parse(yyyymmdd, credElements[0]); err != nil {
+		return accessKey, fmt.Errorf("invalid credential date %s", err)
+	}
+
+	// Region is set to be empty, we use whatever was sent by the
+	// request and proceed further. This is a work-around to address
+	// an important problem for ListBuckets() getting signed with
+	// different regions.
+	if region == "" {
+		region = credElements[1]
+	}
+
+	// Should validate region, only if region is set.
+	if !isValidRegion(credElements[1], region) {
+		return accessKey, errors.New("invalid region")
+
+	}
+	switch serviceType(credElements[2]) {
+	case serviceSTS:
+	case serviceS3:
+	default:
+		return accessKey, fmt.Errorf("invalid service type %s", credElements[2])
+	}
+	if credElements[3] != "aws4_request" {
+		return accessKey, errors.New("invalid AWS signature version")
+	}
+	return accessKey, nil
+}
+
+type serviceType string
+
+const (
+	serviceS3  serviceType = "s3"
+	serviceSTS serviceType = "sts"
+)
+
+// AWS Signature Version '4' constants.
+const (
+	signV4Algorithm = "AWS4-HMAC-SHA256"
+	iso8601Format   = "20060102T150405Z"
+	yyyymmdd        = "20060102"
+)
+
+func getReqAccessKey(r *http.Request, region string) (string, error) {
+	accessKey, err := parseCredentialHeader("Credential="+r.URL.Query().Get(xhttp.AmzCredential), region)
+	if err != nil {
+		// Strip off the Algorithm prefix.
+		v4Auth := strings.TrimPrefix(r.Header.Get("Authorization"), signV4Algorithm)
+		authFields := strings.Split(strings.TrimSpace(v4Auth), ",")
+		if len(authFields) != 3 {
+			return accessKey, errors.New("missing expected fields")
+		}
+		accessKey, err = parseCredentialHeader(authFields[0], region)
+	}
+	return accessKey, err
+}
+
 func main() {
 	flag.Parse()
 
 	defer globalDNSCache.Stop()
+
+	r, err := os.Open(backend)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d := json.NewDecoder(r)
+	if err = d.Decode(&tenantAccessMapping); err != nil {
+		log.Fatal(err)
+	}
+
+	r.Close()
 
 	certs, err := ParsePublicCertFile(caCert)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	origin, err := url.Parse(backend)
-	if err != nil {
-		log.Fatal(err)
-	}
+	secureBackend := len(caCert) > 0
 
 	rootCAs, _ := x509.SystemCertPool()
 	if rootCAs == nil {
@@ -133,20 +239,32 @@ func main() {
 		RootCAs: rootCAs,
 	}, rest.DefaultTimeout)()
 
-	director := func(req *http.Request) {
-		req.Header.Add("X-Forwarded-Host", req.Host)
-		req.Header.Add("X-Forwarded-Proto", origin.Scheme)
-		req.Header.Add("X-Real-IP", req.RemoteAddr)
-		req.URL.Scheme = origin.Scheme
-		req.URL.Host = origin.Host
-	}
-
 	proxy := &httputil.ReverseProxy{
-		Director:  director,
 		Transport: transport,
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		accessKey, err := getReqAccessKey(r, "") // TODO support region
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		r.Header.Add("X-Forwarded-Host", r.Host)
+		r.Header.Add("X-Real-IP", r.RemoteAddr)
+		if secureBackend {
+			r.URL.Scheme = "https"
+		} else {
+			r.URL.Scheme = "http"
+		}
+
+		tenantHost, ok := tenantAccessMapping[accessKey]
+		if !ok {
+			http.Error(w, fmt.Sprintf("no tenant found for accessKey %s", accessKey), http.StatusBadRequest)
+			return
+		}
+
+		r.URL.Host = tenantHost
 		proxy.ServeHTTP(w, r)
 	})
 
